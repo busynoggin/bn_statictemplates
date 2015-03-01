2f636a9934399f9b6b9362b6a3e11fa104f1b50d
Jeff Segars <jeff@busynoggin.com>
2015-02-28 21:42:31 -0600

    Adjust hook to namespaced path

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,6 @@ if (!defined('TYPO3_MODE')) {
 }
 
 // Process non-extension sources of static templates
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSourcesAtEnd'][] = 'EXT:bn_statictemplates/class.tx_bnstatictemplates_lib.php:&tx_bnstatictemplates_lib->includeStaticTemplates';
-
+$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSourcesAtEnd'][] = \BusyNoggin\BnStatictemplates\StaticTemplateLibrary::class . '->includeStaticTemplates';
 
 ?>
